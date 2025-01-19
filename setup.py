b445870c507a317e979592a932be1f63ca4a6140
9b <brandon@9bplus.com>
2025-01-19 16:59:26 -0500

    Initial publish

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='fellow-aiden',
+    version='0.1.0',
+    description='Interface for interacting with Fellow Aiden coffee brewer.
+    url="https://github.com/9b/fellow-aiden",
+    author="Brandon Dixon",
+    author_email="brandon@9bplus.com",
+    license="GNUV3",
+    packages=find_packages(),
+    install_requires=['requests'],
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ],
+    package_data={
+        'fellow_aiden': [],
+    },
+    entry_points={
+        'console_scripts': [
+            'pyhottop-test = pyhottop.cli.config:main'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    keywords=['coffee', 'coffee brewer', 'fellow', 'coffee tech']
+)
